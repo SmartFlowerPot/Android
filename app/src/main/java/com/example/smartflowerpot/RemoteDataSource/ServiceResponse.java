f528624f20ddcb502e5d287fe3be64d2bac43ae4
Ionut Grosu <ionutflorentin.grosu@gmail.com>
2021-11-10 10:09:30 +0100

    Inserted the API URL

--- app/src/main/java/com/example/smartflowerpot/RemoteDataSource/ServiceResponse.java
+++ app/src/main/java/com/example/smartflowerpot/RemoteDataSource/ServiceResponse.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceResponse {
-    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl("https://pokeapi.co").addConverterFactory(GsonConverterFactory.create());
+    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl("https://smartflowerpot.azurewebsites.net/swagger/index.html").addConverterFactory(GsonConverterFactory.create());
     private static Retrofit retrofit = retrofitBuilder.build();
     private static PlantAPI plantApi = retrofit.create(PlantAPI.class);
     private static ServiceResponse instance;
