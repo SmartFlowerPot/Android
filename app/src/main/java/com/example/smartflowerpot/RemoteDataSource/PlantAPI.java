35bb165115bc3fa393d2b3faa1cccfb6daf10857
Leppniz <leppniz@gmail.com>
2021-12-07 11:30:15 +0100

    CO2 Setup #2

--- app/src/main/java/com/example/smartflowerpot/RemoteDataSource/PlantAPI.java
+++ app/src/main/java/com/example/smartflowerpot/RemoteDataSource/PlantAPI.java
@@ -6,9 +6,11 @@ import com.example.smartflowerpot.Model.Account;
 import com.example.smartflowerpot.Model.CO2;
 import com.example.smartflowerpot.RemoteDataSource.Response.AccountResponse;
 import com.example.smartflowerpot.RemoteDataSource.Response.CO2Response;
+import com.example.smartflowerpot.RemoteDataSource.Response.HumidityResponse;
 import com.example.smartflowerpot.RemoteDataSource.Response.TemperatureResponse;
 
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
 import retrofit2.http.POST;
@@ -29,15 +31,23 @@ public interface PlantAPI {
     @POST("Account")
     Call<AccountResponse> registerAccount(@Body Account account);
 
+    @DELETE("Account")
+    Call<AccountResponse> deleteAccount(@Path("username") String username);
+
     //-----------------------------------------------------------------------------------------------
 
     @GET("Temperature")
-    Call<TemperatureResponse> getTemperature();
+    Call<TemperatureResponse> getTemperature(@Query("eui") String eui);
 
     //-----------------------------------------------------------------------------------------------
 
     @GET("CO2")
     Call<CO2Response> getCO2(@Query("eui") String eui);
+
+    //-----------------------------------------------------------------------------------------------
+
+    @GET("Humidity")
+    Call<HumidityResponse> getHumidity(@Query("eui") String eui);
 
 
 
---
+++ app/src/main/java/com/example/smartflowerpot/RemoteDataSource/Response/HumidityResponse.java
@@ -0,0 +1,4 @@
+package com.example.smartflowerpot.RemoteDataSource.Response;
+
+public class HumidityResponse {
+}
