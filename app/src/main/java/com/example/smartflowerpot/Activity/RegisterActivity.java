724461b7b1d2c1378107947eb8abcfb1d15a9842
Ionut Grosu <ionutflorentin.grosu@gmail.com>
2021-12-08 16:33:20 +0100

    Added new input fields to the register layout By Ionut and Cezary

--- app/src/main/java/com/example/smartflowerpot/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/smartflowerpot/Activity/RegisterActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,10 +21,20 @@ import com.example.smartflowerpot.R;
 import com.example.smartflowerpot.ViewModel.AccountViewModel;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
 public class RegisterActivity extends AppCompatActivity {
     private Button registerButton;
     private TextInputEditText usernameInputRegister;
     private TextInputEditText passwordInputRegister;
+    private DatePicker dobDatePicker;
+    private Spinner regionSpinner;
+    private Spinner genderSpinner;
     private AccountViewModel accountViewModel;
 
     @Override
@@ -45,11 +58,57 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
         usernameInputRegister = findViewById(R.id.usernameInputRegister);
         passwordInputRegister = findViewById(R.id.passwordInputRegister);
+        dobDatePicker = findViewById(R.id.dobDatePicker);
+        regionSpinner = findViewById(R.id.regionSpinner);
+        genderSpinner = findViewById(R.id.genderSpinner);
+
+        populateSpinners();
+    }
+
+    private void populateSpinners() {
+        List<String> regions = new ArrayList<>();
+        regions.add("North America");
+        regions.add("South America");
+        regions.add("Europe");
+        regions.add("Asia");
+        regions.add("Australia");
+        regions.add("Africa");
+
+        ArrayAdapter<String> regionAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, regions);
+
+        regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        regionSpinner.setAdapter(regionAdapter);
+
+        List<String> genders = new ArrayList<>();
+        regions.add("Male");
+        regions.add("Female");
+        regions.add("Other");
+
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, genders);
+
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        genderSpinner.setAdapter(genderAdapter);
     }
 
     private void handleRegister() {
         String username = usernameInputRegister.getText().toString().trim();
         String password = passwordInputRegister.getText().toString().trim();
+
+        int day = dobDatePicker.getDayOfMonth() + 1;
+        int month = dobDatePicker.getMonth();
+        int year = dobDatePicker.getYear() - 1900;
+
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'.000'");
+        df.setTimeZone(tz);
+        String nowAsISO = df.format(new Date(year, month, day, 0,0,0));
+        System.out.println(nowAsISO);
+
+
+
+
 
         if (username.isEmpty()) {
             usernameInputRegister.setError("Username cannot be empty.");
