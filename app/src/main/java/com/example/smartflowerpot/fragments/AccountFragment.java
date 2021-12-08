db21e0af127fc064689bbca64e0ccc6e7d62dfbd
Ionut Grosu <ionutflorentin.grosu@gmail.com>
2021-12-08 15:01:08 +0100

    Added logout button By Ionut and Cezary

--- app/src/main/java/com/example/smartflowerpot/fragments/AccountFragment.java
+++ app/src/main/java/com/example/smartflowerpot/fragments/AccountFragment.java
@@ -1,5 +1,8 @@
 package com.example.smartflowerpot.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +10,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.smartflowerpot.Activity.BaseActivity;
+import com.example.smartflowerpot.Activity.LoginActivity;
 import com.example.smartflowerpot.R;
 
 public class AccountFragment extends Fragment {
+
+    private Button logoutbtn;
 
 
     @Override
@@ -23,6 +30,33 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        initViews(view);
+
+        logoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleLogout();
+            }
+        });
+
+        return view;
+    }
+
+    private void handleLogout() {
+        SharedPreferences prefs = getActivity().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.remove("username");
+        editor.apply();
+
+        Intent intent = new Intent(getContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void initViews(View view) {
+        logoutbtn = view.findViewById(R.id.logoutBtn);
     }
 }
