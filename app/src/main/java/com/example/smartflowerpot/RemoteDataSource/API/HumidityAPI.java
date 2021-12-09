84446a82354f80c30c61072d213035723ddf18c0
Ionut Grosu <ionutflorentin.grosu@gmail.com>
2021-12-09 20:07:57 +0100

    Moved rest api calls from HumidityRepo to HumidityAPI By Ionut and Cezary

---
+++ app/src/main/java/com/example/smartflowerpot/RemoteDataSource/API/HumidityAPI.java
@@ -0,0 +1,91 @@
+package com.example.smartflowerpot.RemoteDataSource.API;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.smartflowerpot.Model.Humidity;
+import com.example.smartflowerpot.RemoteDataSource.ApplicationAPI;
+import com.example.smartflowerpot.RemoteDataSource.Response.HumidityResponse;
+import com.example.smartflowerpot.RemoteDataSource.ServiceGenerator;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class HumidityAPI {
+    private static HumidityAPI instance;
+
+    private MutableLiveData<Humidity> humidity;
+    private MutableLiveData<ArrayList<Humidity>> humidityArray;
+
+    private HumidityAPI(){
+        humidity = new MutableLiveData<>();
+    }
+
+    public static HumidityAPI getInstance() {
+        if (instance == null){
+            instance = new HumidityAPI();
+        }
+        return instance;
+    }
+
+    public LiveData<ArrayList<Humidity>> getWeekHumidity() {
+        return humidityArray;
+    }
+
+    public LiveData<Humidity> getHumidity() {
+        return humidity;
+    }
+
+    public void getWeekHumidityRequest(String deviceID) {
+        ApplicationAPI applicationAPI = ServiceGenerator.getPlantAPI();
+        Call<HumidityResponse> call = applicationAPI.getHumidity(deviceID);
+        call.enqueue(new Callback<HumidityResponse>() {
+            @Override
+            public void onResponse(Call<HumidityResponse> call, Response<HumidityResponse> response) {
+                if (response.isSuccessful()) {
+                    if (response.code() == 204) {
+                        humidityArray.setValue(null);
+                    } else {
+
+                        //TODO
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HumidityResponse> call, Throwable t) {
+                Log.i("Retrofit", "Something went wrong");
+            }
+        });
+    }
+
+    public void getHumidityRequest(String deviceID) {
+        ApplicationAPI applicationAPI = ServiceGenerator.getPlantAPI();
+        Call<HumidityResponse> call = applicationAPI.getHumidity(deviceID);
+        call.enqueue(new Callback<HumidityResponse>() {
+            @Override
+            public void onResponse(Call<HumidityResponse> call, Response<HumidityResponse> response) {
+                if (response.isSuccessful()) {
+                    if (response.code() == 204) {
+                        humidity.setValue(null);
+                    } else {
+                        System.out.println(response.body().getHumidity());
+                        humidity.setValue(response.body().getHumidity());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<HumidityResponse> call, Throwable t) {
+                Log.i("Retrofit", "Something went wrong");
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/smartflowerpot/Repository/HumidityRepo.java
+++ app/src/main/java/com/example/smartflowerpot/Repository/HumidityRepo.java
@@ -1,92 +1,43 @@
 package com.example.smartflowerpot.Repository;
 
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.example.smartflowerpot.Model.Humidity;
-import com.example.smartflowerpot.RemoteDataSource.ApplicationAPI;
-import com.example.smartflowerpot.RemoteDataSource.Response.HumidityResponse;
-import com.example.smartflowerpot.RemoteDataSource.ServiceGenerator;
+import com.example.smartflowerpot.RemoteDataSource.API.HumidityAPI;
 
 import java.util.ArrayList;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 //  Ionut
 
 public class HumidityRepo {
     private static HumidityRepo instance;
-    private MutableLiveData<Humidity> humidity;
-    private MutableLiveData<ArrayList<Humidity>> humidityArray;
+    private HumidityAPI humidityAPI;
 
     private HumidityRepo() {
-        humidity = new MutableLiveData<>();
+        humidityAPI = HumidityAPI.getInstance();
     }
 
     public static HumidityRepo getInstance() {
         if (instance == null) {
             instance = new HumidityRepo();
         }
-
         return instance;
     }
 
     public LiveData<ArrayList<Humidity>> getWeekHumidity() {
-        return humidityArray;
+        return humidityAPI.getWeekHumidity();
     }
 
     public LiveData<Humidity> getHumidity() {
-        return humidity;
+        return humidityAPI.getHumidity();
     }
 
     public void getWeekHumidityRequest(String deviceID) {
-        ApplicationAPI applicationAPI = ServiceGenerator.getPlantAPI();
-        Call<HumidityResponse> call = applicationAPI.getHumidity(deviceID);
-        call.enqueue(new Callback<HumidityResponse>() {
-            @Override
-            public void onResponse(Call<HumidityResponse> call, Response<HumidityResponse> response) {
-                if (response.isSuccessful()) {
-                    if (response.code() == 204) {
-                        humidityArray.setValue(null);
-                    } else {
-
-                        //TODO
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<HumidityResponse> call, Throwable t) {
-                Log.i("Retrofit", "Something went wrong");
-            }
-        });
+        humidityAPI.getWeekHumidityRequest(deviceID);
     }
 
     public void getHumidityRequest(String deviceID) {
-        ApplicationAPI applicationAPI = ServiceGenerator.getPlantAPI();
-        Call<HumidityResponse> call = applicationAPI.getHumidity(deviceID);
-        call.enqueue(new Callback<HumidityResponse>() {
-            @Override
-            public void onResponse(Call<HumidityResponse> call, Response<HumidityResponse> response) {
-                if (response.isSuccessful()) {
-                    if (response.code() == 204) {
-                        humidity.setValue(null);
-                    } else {
-                        System.out.println(response.body().getHumidity());
-                        humidity.setValue(response.body().getHumidity());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<HumidityResponse> call, Throwable t) {
-                Log.i("Retrofit", "Something went wrong");
-            }
-        });
+        humidityAPI.getHumidityRequest(deviceID);
     }
 
 }
